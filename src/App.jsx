f807393fea8d1e77637203da0b6e7462607e73db
Jedidiah Musa <jedidiahsimon3@gmail.com>
2025-05-20 16:11:08 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main className="min-h-screen w-screen overflow-x-hidden relative ">
+      <Navbar />
       <Hero />
-      <section className="min-h-screen " />
+      <About />
+    
     </main>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+import Button from "./Button";
+
+
+const navaitems = [
+  'Nexus', 'Vault',  'Prolouge', 'About', 'Contact'
+]
+const toggleAudioIndicator = () => {}
+const audioElementRef = useRef(null) 
+
+function Navbar() {
+  const navContainerRef = useRef(null);
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 z-50 top-4 h-16 border-0 transition-all duration-700 sm:inset-x-6 "
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2 ">
+        <nav className="flex size-full items-center justify-between p-4 ">
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="" className="w-10" />
+            <Button
+              id="product-button"
+              title="products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-50 md:flex items-center hidden justify-center gap-1 "
+            />
+          </div>
+          <div className="flex h-full items-center ">
+            <div className="hidden md:block ">
+              {navaitems.map((item)=>(
+                <a key={item} className="nav-hover-btn" href={`#${item.toLowerCase()}`} >{item}</a>
+              ))}
+            </div>
+            <button onClick={toggleAudioIndicator}  className="ml-10 flex items-center space-x-0.5 ">
+              <audio ref={audioElementRef} className="hidden " src="/audio/loop/mp3"></audio>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
