63d1be6f024ef8685112453b7c5d66a24d516ef0
Jedidiah Musa <jedidiahsimon3@gmail.com>
2025-05-25 13:49:18 +0100

    .

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -2,33 +2,33 @@ import gsap from "gsap";
 import React, { useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/all";
 
-
-gsap.registerPlugin(ScrollTrigger)
-
-
+gsap.registerPlugin(ScrollTrigger);
 
 const AnimatedTitle = ({ title, containerClass }) => {
-const containerRef = useRef(null)
-useEffect(() => {
-  const ctx = gsap.context(() => {
-    const titleAnimation = gsap.timeline({
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: "100 bottom",
-        end: "center bottom",
-        toggleActions: "play none none reverse"
-      }
-    });
-    titleAnimation.to(".animated-word",{
-      opacity: 1,
-      transform: 'translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg) ',
-      ease: 'power2.inOut',
-      stagger: 0.02,
-    },0);
-  }, containerRef);
-  return () => ctx.revert();
-},[]);
-
+  const containerRef = useRef(null);
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bottom",
+          toggleActions: "play none none reverse",
+        },
+      });
+      titleAnimation.to(
+        ".animated-word",
+        {
+          opacity: 1,
+          transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg) ",
+          ease: "power2.inOut",
+          stagger: 0.02,
+        },
+        0
+      );
+    }, containerRef);
+    return () => ctx.revert();
+  }, []);
 
   return (
     <div ref={containerRef} className={`animated-title ${containerClass}`}>
@@ -48,6 +48,6 @@ useEffect(() => {
       ))}
     </div>
   );
-}
+};
 
 export default AnimatedTitle;
