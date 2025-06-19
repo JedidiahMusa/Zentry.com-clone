999054a4e92bf084a5c32b82b8067c4427ed5728
JedidiahMusa <jedidiahsimon3@gmail.com>
2025-06-19 15:05:01 +0100

    final touch before deploying

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -99,7 +99,7 @@ export const BentoCard = ({ src, title, description, isComingSoon }) => {
 };
 
 const Features = () => (
-  <section className="bg-black pb-52">
+  <section className="bg-black pb-20">
     <div className="container mx-auto px-3 md:px-10">
       <div className="px-5 py-32">
         <p className="font-circular-web text-lg text-blue-50">
@@ -125,8 +125,8 @@ const Features = () => (
         />
       </BentoTilt>
 
-      <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
-        <BentoTilt className=" row-span-1 md:col-span-1 md:row-span-2">
+      <div className="md:grid h-[135vh] flex flex-col w-full grid-cols-2 grid-rows-3 gap-7">
+        <BentoTilt className="h-full md:col-span-1 md:row-span-2">
           <BentoCard
             src="videos/feature-2.mp4"
             title={
@@ -139,7 +139,7 @@ const Features = () => (
           />
         </BentoTilt>
 
-        <BentoTilt className=" row-span-1 ms-32 md:col-span-1 md:ms-0">
+        <BentoTilt className="w-full h-full row-span-1 md:col-span-1 md:ms-0">
           <BentoCard
             src="videos/feature-3.mp4"
             title={
@@ -152,7 +152,7 @@ const Features = () => (
           />
         </BentoTilt>
 
-        <BentoTilt className=" row-span-1 me-14 md:col-span-1 md:me-0">
+        <BentoTilt className="w-full h-full row-span-1 me-14 md:col-span-1 md:me-0">
           <BentoCard
             src="videos/feature-4.mp4"
             title={
@@ -165,7 +165,8 @@ const Features = () => (
           />
         </BentoTilt>
 
-        <BentoTilt className="bento-tilt_2">
+        <div className="flex gap-5 col-span-2" >
+          <BentoTilt className="w-full h-full relative col-span-1 row-span-1 overflow-hidden rounded-md transition-transform duration-300 ease-out ">
           <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
             <h1 className="bento-title special-font max-w-64 text-black">
               M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
@@ -175,7 +176,7 @@ const Features = () => (
           </div>
         </BentoTilt>
 
-        <BentoTilt className="bento-tilt_2">
+        <BentoTilt className="w-full h-full bento-tilt_2">
           <video
             src="videos/feature-5.mp4"
             loop
@@ -184,6 +185,9 @@ const Features = () => (
             className="size-full object-cover object-center"
           />
         </BentoTilt>
+        </div>
+
+        
       </div>
     </div>
   </section>
